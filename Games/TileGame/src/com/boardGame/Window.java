e7b5a0c3bfdb2728089e951cdedbd464b1d92344
AbhilashTUofficial <abhilashstorm@gmail.com>
2020-10-08 02:15:27 +0530

    Logo added

--- Games/TileGame/src/com/boardGame/Window.java
+++ Games/TileGame/src/com/boardGame/Window.java
@@ -1,16 +1,23 @@
 package com.boardGame;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Window {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int x=600;
         int y=100;
         int width=610;
         int height=620;
+        String imagePath="Assets/Img/TileGame.png";
+        BufferedImage logo= ImageIO.read(new File(imagePath));
 	JFrame win=new JFrame();
 	Board board=new Board();
+	win.setIconImage(logo);
 	win.setBounds(x,y,width,height);
 	win.setTitle("Board Game");
 	win.setVisible(true);
